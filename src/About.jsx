bff6b2f8128fdfd785fab03190faff33f42c47cb
sundar raj <64121348+sundarraj33@users.noreply.github.com>
2025-02-20 11:40:14 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
             <img src="https://5.imimg.com/data5/SELLER/Default/2023/6/314195439/VJ/ON/QM/3317023/panai-karupatti-palm-jaggery-1000x1000.jpg" />
           </div>
           <div className="box">
-            <img src="https://media.istockphoto.com/id/1440003815/photo/hospital-colleagues-checking-medical-records-database.jpg?s=2048x2048&w=is&k=20&c=cHDOSiDM84djbIFrzXgLLxBYYLp7yoSv5Z-8VvpiyrQ=" />
+            <img src="https://ueirorganic.com/cdn/shop/files/topsellingtag_9.png?v=1685772233&width=600" />
           </div>
           <div className="box">
             <p>is a traditional South Indian beverage that has gained popularity for its unique flavor and numerous health benefits.Made from palm jaggery and coffee, this drink offers a delightful combination of sweetness and bitterness</p>
