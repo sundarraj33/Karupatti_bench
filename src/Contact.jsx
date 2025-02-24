fa3f72a82e8d4eebfc57209af2aa1f3ca30b7f10
sundarraj33 <sundarraj.m033@gmail.com>
2025-02-24 23:47:11 +0530

    Upload Contact Box page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -9,28 +9,38 @@ const Contact = ()=>{
             <div className="container">
                 <div className="contact_container">
                     <div className="contact1">
-                        <p>
-                            Karupatti Bench <br></br>
-                            <p>Guindy <br></br>
-                            Chennai</p>
-                        </p>
-                    </div>
-                    <div className="contact2">
-                        <div className="contact_box">
-                            <div className="contact_field">
-                                <label>Name</label>
-                                <input type='text' name="" placeholder="Enter Your Name"></input>
-                            </div>
-                            <div className="contact_field">
-                                <label>Name</label>
-                                <input type='text' name="" placeholder="Enter Your Name"></input>
-                            </div>
-                            <div className="contact_field">
-                                <label>Name</label>
-                                <input type='text' name="" placeholder="Enter Your Name"></input>
-                            </div>
+                        <h3>Karupatti Bench</h3>
+                        <div className="">
+                            <h4>Location:</h4>
+                            <p>CTI Campus, TVK Industrial Estate,<br></br>
+                            Guindy, Chennai - 600032 Tamil Nadu, India</p>
+                        </div>
+                        <div className="">
+                            <h4>Email:</h4>
+                            <p>Sundarrajcs1@gmail.com</p>
+                        </div>
+                        <div className="">
+                            <h4>Call & Whatsapp:</h4>
+                            <p>9486947738</p>
                         </div>
                     </div>
+                    <div className="contact2">         
+                    <h2>Feedback</h2>
+                    <p>You are encouraged to use the online feedback form below, to send us your valuable comments and suggestions as well as any queries about our products and services.</p>               
+                        <div className="contact_field">                        
+                            <label>Name</label>
+                            <input type='text' name="" placeholder="Enter Your Name"></input>
+                        </div>
+                        <div className="contact_field">
+                            <label>Email</label>
+                            <input type='text' name="" placeholder="Enter Your Name"></input>
+                        </div>
+                        <div className="contact_field">
+                            <label>Mobile</label>
+                            <input type='text' name="" placeholder="Enter Your Name"></input>
+                        </div>
+                        <button>Submit</button>                        
+                </div>
                 </div>
             </div>
 
